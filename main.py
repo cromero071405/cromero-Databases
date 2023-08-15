46ef39432e05a4e3b5017cfe07b269c0ccddd7e6
cromero071405 <cesardr1@uci.edu>
2023-08-15 12:48:41 -0700

    Created a connection to the Major League Soccer Database. Created a cursor object to be able to work with the Database.

---
+++ main.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+# Connecting to the MLS (Major League Soccer) Database
+connection_object = sqlite3.connect("Major_League_Soccer.db")
+
+# Creating cursor object to be able to execute and play with the queries in the database
+curser_object = connection_object.cursor()
